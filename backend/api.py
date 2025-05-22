aa6c99d449d20a4942b2981dd2fdc57274568aeb
Quackvin <kevin.kuek.95@gmail.com>
2025-05-22 15:16:36 +0800

    allow traffic from texo http

--- backend/api.py
+++ backend/api.py
@@ -109,7 +109,7 @@ async def log_requests_middleware(request: Request, call_next):
         raise
 
 # Define allowed origins based on environment
-allowed_origins = ["https://app.texoai.com.au", "http://localhost:3000"]
+allowed_origins = ["https://app.texoai.com.au", "http://app.texoai.com.au","http://localhost:3000"]
 
 # Add staging-specific origins
 if config.ENV_MODE == EnvMode.STAGING:
